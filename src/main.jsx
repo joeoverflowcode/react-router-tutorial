7899e8448a0d99fac4aa185cb26de2785bafbb49
Joe Aguado <aguado.joe@gmail.com>
2024-05-22 17:02:27 -0500

    vite config

--- src/main.jsx
+++ src/main.jsx
@@ -59,6 +59,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <RouterProvider router={router}/>
+
   </React.StrictMode>,
 )
